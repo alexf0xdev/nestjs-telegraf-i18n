93b606bb5f04ebcb59ea0fa442a6b76ac4f181b7
sgerodes <sgerodes@gmail.com>
2025-03-04 21:13:43 +0100

    feat: expose I18nContextGetterPatcher via TelegrafI18nModule exports

--- src/telegraf-i18n.module.ts
+++ src/telegraf-i18n.module.ts
@@ -1,10 +1,10 @@
 import { Module, Global } from '@nestjs/common';
 import { TelegrafI18nMiddlewareProvider } from './telegraf-i18n.middleware';
-import { I18nContextGetterPatcher, TelegrafI18nContextGetterPatcher } from "./i18n-context-patcher";
+import { TelegrafI18nContextGetterPatcher } from "./i18n-context-patcher";
 
 @Global()
 @Module({
     providers: [TelegrafI18nMiddlewareProvider, TelegrafI18nContextGetterPatcher],
-    exports: [TelegrafI18nMiddlewareProvider, TelegrafI18nContextGetterPatcher, I18nContextGetterPatcher],
+    exports: [TelegrafI18nMiddlewareProvider, TelegrafI18nContextGetterPatcher],
 })
 export class TelegrafI18nModule {}
