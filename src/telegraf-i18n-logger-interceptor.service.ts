43a8844928aeb207b8d65f39939751dab21ccf58
sgerodes <sgerodes@gmail.com>
2025-03-04 20:35:03 +0100

    feat: add warning suppressor for unsupported i18n context types

---
+++ src/telegraf-i18n-logger-interceptor.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+
+@Injectable()
+export class TelegrafI18nLoggerInterceptor implements OnModuleInit {
+    private readonly logger = new Logger(this.constructor.name);
+    private static readonly suppressed_messages: string = 'context type: telegraf not supported';
+
+    onModuleInit() {
+        this.logger.log(`Intercepting I18nService Logger to suppress unwanted warnings: '${TelegrafI18nLoggerInterceptor.suppressed_messages}'`);
+        const originalWarn = Logger.prototype.warn;
+        Logger.prototype.warn = function (message: any, ...optionalParams: any[]) {
+            if (typeof message === 'string' && message.startsWith(TelegrafI18nLoggerInterceptor.suppressed_messages)) {
+                return; // Suppress this specific warning
+            }
+            originalWarn.call(this, message, ...optionalParams);
+        };
+    }
+}
--- src/telegraf-i18n.module.ts
+++ src/telegraf-i18n.module.ts
@@ -1,9 +1,10 @@
 import { Module, Global } from '@nestjs/common';
 import { TelegrafI18nMiddlewareProvider } from './telegraf-i18n.middleware';
+import {TelegrafI18nContextGetterPatcher} from "./I18n-context-getter-patcher";
 
 @Global()
 @Module({
-    providers: [TelegrafI18nMiddlewareProvider],
+    providers: [TelegrafI18nMiddlewareProvider, TelegrafI18nContextGetterPatcher],
     exports: [TelegrafI18nMiddlewareProvider],
 })
 export class TelegrafI18nModule {}
